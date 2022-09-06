dd764dce874b1b8d67d48752c13e5abb6f1b3851
PawelHerma <92121734+PawelHerma@users.noreply.github.com>
2022-09-06 12:03:15 +0200

    fix

--- DbModels/Member.cs
+++ DbModels/Member.cs
@@ -12,7 +12,7 @@ namespace Project_P4.DbModels
 
         public int MemberId { get; set; }
         public string? MemberName { get; set; }
-        public decimal? MemberBudget { get; set; }
+        public decimal MemberBudget { get; set; }
 
         public virtual ICollection<Expence> Expences { get; set; }
     }
--- DbModels/Projekt01_HermaContext.cs
+++ DbModels/Projekt01_HermaContext.cs
@@ -26,7 +26,7 @@ namespace Project_P4.DbModels
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=Projekt01_Herma;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=Projekt001_Herma;Trusted_Connection=True;");
             }
         }
 
